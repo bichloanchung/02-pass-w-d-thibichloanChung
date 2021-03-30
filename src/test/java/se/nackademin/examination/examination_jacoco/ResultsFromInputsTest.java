d53ead980091eb114bcd9990ba9ef659d0737cee
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-30 16:37:35 +0200

    start test ResultsFromInputsTest

--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -7,10 +7,25 @@ import org.junit.Test;
 public class ResultsFromInputsTest {
 
 	@Test
-	public void testAgeGettersAndSettersMethods() {
+	public void testNameGettersAndSettersMethods() {
 		ResultFromInputs resultsFromInputs = new ResultFromInputs();
 		resultsFromInputs.setResultForNameLenght(10);
 		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
 	}
+	
+	@Test
+	public void testAgeGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForAge(2);
+		assertEquals("The result should be 2", resultsFromInputs.getResultForAge(), 2);
+	}
+	
+	@Test
+	public void testGenderGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForGender(1);
+		assertEquals("The result should be 1", resultsFromInputs.getResultForGender(), 1);
+	}
+	
 
 }
