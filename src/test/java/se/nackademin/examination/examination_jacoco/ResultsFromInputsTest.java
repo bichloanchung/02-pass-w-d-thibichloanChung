5c4a3adf6525cb2ee5c3cff46f995b70548a04ec
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-30 18:16:53 +0200

    ResultsFromInputsTest done

--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -16,15 +16,29 @@ public class ResultsFromInputsTest {
 	@Test
 	public void testAgeGettersAndSettersMethods() {
 		ResultFromInputs resultsFromInputs = new ResultFromInputs();
-		resultsFromInputs.setResultForAge(2);
-		assertEquals("The result should be 2", resultsFromInputs.getResultForAge(), 2);
+		resultsFromInputs.setResultForAge(24);
+		assertEquals("The result should be 24", resultsFromInputs.getResultForAge(), 24);
+		
+		resultsFromInputs.setResultForAge(30);
+		assertEquals("The result should be 30", resultsFromInputs.getResultForAge(), 30);
 	}
 	
 	@Test
 	public void testGenderGettersAndSettersMethods() {
 		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		
+		resultsFromInputs.setResultForGender(0);
+		assertEquals("The result should be 0", resultsFromInputs.getResultForGender(), 0);
+		
 		resultsFromInputs.setResultForGender(1);
 		assertEquals("The result should be 1", resultsFromInputs.getResultForGender(), 1);
+	}
+	
+	@Test
+	public void testCityGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForHomeCity(8);;
+		assertEquals("The result should be 8", resultsFromInputs.getResultForHomeCity(), 8);
 	}
 	
 
