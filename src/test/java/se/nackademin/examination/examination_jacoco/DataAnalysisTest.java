a400e8c925740a08c7245e45ac5b680b48c46072
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-30 21:33:28 +0200

    DataAnalysisTest done

--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -63,5 +63,16 @@ public class DataAnalysisTest {
 				result.contains("The first name is smaller or equals in size to the last name and the participant is 30 or younger"));
 
 	}
+	
+	@Test
+	public void test6() {
+		DataAnalysis dataAnalysis = new DataAnalysis();
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Elina", "Yang", "F", "50", "Paris"));
+		String result = dataAnalysis.buildFinalString(values);
+		assertTrue("The result should contain 'The first name is greater or equals in size to the last name and the participant older than 30' ",
+				result.contains("The first name is greater or equals in size to the last name and the participant older than 30"));
+
+	}
 
 }
